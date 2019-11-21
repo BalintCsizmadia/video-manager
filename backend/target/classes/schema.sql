4fdb7b081fdf103339fccfaf924424ad1ebb4c61
Balint Csizmadia <balintcsizmadia1@gmail.com>
2019-11-21 21:20:58 +0100

    Cleanup

--- backend/target/classes/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS videos;
-
-CREATE TABLE videos (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE,
-    duration TEXT NOT NULL,
-    description TEXT,
-    url TEXT NOT NULL
-)
