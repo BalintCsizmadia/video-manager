fba166eca29f451349ed712e23460447c3686100
Balint Csizmadia <balintcsizmadia1@gmail.com>
2019-11-23 16:29:12 +0100

    Implement CRUD functions

--- frontend/src/application/main/Main.tsx
+++ frontend/src/application/main/Main.tsx
@@ -2,10 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./Main.css";
 import axios from "axios";
 import Grid from "@material-ui/core/Grid";
-import Playlist from "../playlist/Playlist";
 import Videos from "../videos/Videos";
 import { Video } from "../utils/Interface";
-import { Typography } from "@material-ui/core";
+import { Typography, Button } from "@material-ui/core";
 
 // set the base URL
 const axiosInst = axios.create({
@@ -18,6 +17,12 @@ const Main: React.FC = () => {
   const [videos, setVideos] = useState<Video[]>([]);
   // videos in playlist
   const [playlistVideos, setPlaylistVideos] = useState<Video[]>([]);
+  // videos to playlist before adding
+  const [inMemoryVideos, setInMemoryVideos] = useState<Video[]>([]);
+  // videos to playlist before removing
+  const [inMemoryVideosToRemove, setInMemoryVideosToRemove] = useState<Video[]>(
+    []
+  );
   // informative messages
   const [message, setMessage] = useState<string>("");
 
@@ -27,19 +32,40 @@ const Main: React.FC = () => {
     return data;
   };
 
-  useEffect(() => {
-    // get all videos from db
-    getVideos()
-      .then((allVideos: Video[]) => {
-        setVideos(allVideos);
-      })
-      .catch((err: any) => {
-        setMessage(err.message);
-      });
-  }, []);
+  const getPlaylistVideos = async () => {
+    const { data } = await axiosInst.get("videos/playlist");
+    return data;
+  };
+
+  // finalize the item addition to playlist
+  const handleUpdatePlaylistClick = async (videoList: Video[]) => {
+    await axiosInst.put("/videos/playlist/", videoList).then(() => {
+      updatePlaylist();
+    });
+  };
+
+  const removeFromPlaylist = async (videoId: number) => {
+    await axiosInst.post(`/videos/playlist/${videoId}`);
+  };
+
+  useEffect(
+    () => {
+      // get all videos from db
+      getVideos()
+        .then((allVideos: Video[]) => {
+          setVideos(allVideos);
+        })
+        .catch((err: any) => {
+          setMessage(err.message);
+        });
+    },
+    [
+      // variable changes here indicates function re-call
+    ]
+  );
 
   // add one video to the playlist
-  const onAddVideoClick = (videoToAdd: Video) => {
+  const addToPlaylist = (videoToAdd: Video) => {
     // check if item already in the list or not
     if (
       !(playlistVideos.filter((v: Video) => v.id === videoToAdd.id).length > 0)
@@ -48,22 +74,100 @@ const Main: React.FC = () => {
     }
   };
 
-  const PlaylistComponent = Playlist({ playlistVideos });
-  const VideosComponent = Videos({ videos, add: onAddVideoClick });
+  // use in VideosComponent when select videos
+  const addVideosToInMemory = (videosToInMemory: Video[]) => {
+    videosToInMemory.map(v => {
+      v.available = true;
+    });
+    setInMemoryVideos(videosToInMemory);
+    // console.log(videosToInMemory);
+  };
+
+  const removeOneFromPlaylist = (videoId: number) => {
+    removeFromPlaylist(videoId).then(() => {
+      updatePlaylist();
+    });
+  };
+
+  const updatePlaylist = () => {
+    getPlaylistVideos().then((playlistVideos: Video[]) => {
+      setPlaylistVideos(playlistVideos);
+    });
+  };
+
+  // FIXME remove multiple videos
+  const removeVideosFromPlaylist = (vids: Video[]) => {
+    vids.map(v => {
+      v.available = false;
+    });
+    setInMemoryVideosToRemove(vids);
+  };
+
+  // UPDATE - with this approach, the whole list loads again
+  // and the checkboxes will be empty
+  // getVideos()
+  //   .then((allVideos: Video[]) => {
+  //     setVideos(allVideos);
+  //     // store videos for playlist
+  //     let tempPlaylist: Video[] = [];
+  //     allVideos.map((v: Video) => {
+  //       if (v.available) {
+  //         tempPlaylist.push(v);
+  //       }
+  //     });
+  //     setPlaylistVideos(tempPlaylist);
+  //   })
+
+  const PlaylistComponent = Videos({
+    videos: playlistVideos,
+    // removeVideosFromPlaylist,
+    removeOneFromPlaylist
+  });
+  const VideosComponent = Videos({ videos, addVideosToInMemory });
 
   return (
     <div id="main-container">
       {message ? (
         <Typography>{message}</Typography>
       ) : (
-        <Grid container>
-          <Grid item xs={12} sm={6}>
-            <div className="video-list">{PlaylistComponent}</div>
+        <div id="video-lists">
+          <Grid container>
+            <Grid item xs={12} sm={6}>
+              <div className="video-list">{PlaylistComponent}</div>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <div className="video-list">{VideosComponent}</div>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <div className="video-list">{VideosComponent}</div>
-          </Grid>
-        </Grid>
+          <div id="operation-buttons">
+            <Grid container>
+              <Grid item xs={12} sm={6}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => {
+                    // add the previously selected items to the playlist
+                    handleUpdatePlaylistClick(inMemoryVideos);
+                  }}
+                >
+                  Add to playlist
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => {
+                    // TODO now I remove one item at a time so it is not working, maybe I should remove it
+                    handleUpdatePlaylistClick(inMemoryVideosToRemove);
+                  }}
+                >
+                  Add new video
+                </Button>
+              </Grid>
+            </Grid>
+          </div>
+        </div>
       )}
     </div>
   );
--- frontend/src/application/videos/Videos.tsx
+++ frontend/src/application/videos/Videos.tsx
@@ -7,23 +7,36 @@ import {
   ListItemText,
   ListItemSecondaryAction,
   List,
-  Paper
+  Paper,
+  Button
 } from "@material-ui/core";
+import { DeleteForeverSharp } from '@material-ui/icons';
 import { Video } from "../utils/Interface";
+import { getVideoTumbnail } from "../utils/GeneralVideoFunction";
+import RemoveContentDialog from "../utils/RemoveContentDialog";
+
 
 interface Props {
   videos: Video[];
-  add(videoToAdd: Video): void;
+  addVideosToInMemory?(tempVideosToAdd: Video[]): void;
+  removeVideosFromPlaylist?(videosToRemove: Video[]): void;
+  removeOneFromPlaylist?(videoId: number): void;
 }
 
 const Videos: React.FC<Props> = (props: Props) => {
-  const [checked, setChecked] = useState([0]);
-  const allVideos: Video[] = props.videos;
+  const [checked, setChecked] = useState<Video[]>([]);
+  const [allVideos, setAllvideos] = useState<Video[]>([]);
+  const [isUserWantToDelete, setUserWantToDelete] = useState(false);
+  const [videoIdToDelete, setVideoIdToDelete] = useState(0);
+  // const allVideos: Video[] = props.videos;
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    setAllvideos(props.videos);
+  }, [props]);
 
-  const handleToggle = (value: any) => () => {
+  const handleToggle = (value: Video) => () => {
     const currentIndex = checked.indexOf(value);
+
     const newChecked = [...checked];
     if (currentIndex === -1) {
       newChecked.push(value);
@@ -31,44 +44,90 @@ const Videos: React.FC<Props> = (props: Props) => {
       newChecked.splice(currentIndex, 1);
     }
     setChecked(newChecked);
+    if (props.addVideosToInMemory) {
+      // save selected videos to a temporary array which is available in the parent function
+      props.addVideosToInMemory(newChecked);
+    } else {
+      if (props.removeVideosFromPlaylist) {
+        console.log("Remove");
+        console.log(newChecked);
+        props.removeVideosFromPlaylist(newChecked);
+      }
+    }
   };
 
-  // get a cover image from the video
-  const getVideoTumbnail = (url: string) => {
-    const urlArray = url.split("/");
-    const youTubeVideoId = urlArray[urlArray.length - 1];
-    return `https://img.youtube.com/vi/${youTubeVideoId}/0.jpg`;
+  const removeOne = (id: number) => {
+    if (props.removeOneFromPlaylist) {
+      props.removeOneFromPlaylist(id);
+    }
   };
+
+  const RemoveDialogComponent = RemoveContentDialog({
+    name: "video",
+    itemIdToDelete: videoIdToDelete,
+    delete: removeOne,
+    setStateBack: () => {
+      setUserWantToDelete(false);
+    }
+  });
 
   return (
     <React.Fragment>
-      <Typography id="title">Available videos</Typography>
+      {props.addVideosToInMemory ? (
+        <Typography id="title">Available videos</Typography>
+      ) : (
+        <Typography id="title">Playlist</Typography>
+      )}
+
       <Paper id="paper">
-        <List>
-          {allVideos.map((video: any) => {
-            const videoId = video.id;
-            return (
-              <ListItem key={videoId} button>
-                <img
-                  id="tumbnail-img"
-                  src={getVideoTumbnail(video.url)}
-                  alt="cover"
-                />
-                <ListItemText id={videoId} primary={video.name} />
-                <ListItemSecondaryAction>
-                  <Checkbox
-                    edge="end"
-                    onChange={handleToggle(video)}
-                    checked={checked.indexOf(video) !== -1}
-                    inputProps={{ "aria-labelledby": videoId }}
+        {allVideos.length > 0 ? (
+          <List>
+            {allVideos.map((video: any) => {
+              const videoId = video.id;
+              return (
+                <ListItem key={videoId} button>
+                  <img
+                    id="tumbnail-img"
+                    src={getVideoTumbnail(video.url)}
+                    alt="cover"
                   />
-                  {/* <button onClick={() => {props.add(video)}}>ADD</button> */}
-                </ListItemSecondaryAction>
-              </ListItem>
-            );
-          })}
-        </List>
+                  <ListItemText id={videoId} primary={video.name} secondary={video.description} />
+                  <ListItemSecondaryAction>
+                    {!props.removeOneFromPlaylist ? (
+                      <Checkbox
+                        edge="end"
+                        onChange={handleToggle(video)}
+                        checked={checked.indexOf(video) !== -1}
+                        inputProps={{ "aria-labelledby": videoId }}
+                        color="primary"
+                      />
+                    ) : (
+                      props.removeOneFromPlaylist && (
+                        <Button
+                          onClick={() => {
+                            setVideoIdToDelete(video.id);
+                            setUserWantToDelete(true);
+                          }}
+                        >
+                          <DeleteForeverSharp color="error" />
+                        </Button>
+                      )
+                    )}
+                  </ListItemSecondaryAction>
+                </ListItem>
+              );
+            })}
+          </List>
+        ) : (
+          // if playlist is empty
+          <ListItem>
+            <Typography>The list is still empty</Typography>
+          </ListItem>
+        )}
       </Paper>
+      {/* REMOVE POP UP */}
+      {isUserWantToDelete && RemoveDialogComponent}
+
     </React.Fragment>
   );
 };
