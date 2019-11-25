4e3f5499ee1f9cc484c38ba7c9fd89ec3d34286c
Balint Csizmadia <balintcsizmadia1@gmail.com>
2019-11-25 13:53:10 +0100

    'description' cannot be Null

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE videos (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
     duration TEXT NOT NULL,
-    description TEXT,
+    description TEXT NOT NULL,
     url TEXT NOT NULL,
     is_available BOOLEAN
 )
