d5b4e64d3fa2fa2b83899071f3a12efd49941f9b
Balint Csizmadia <balintcsizmadia1@gmail.com>
2019-11-25 13:26:35 +0100

    Add new route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Main from "./application/main/Main";
 import { Typography } from "@material-ui/core";
+import AddVideoForm from "./application/videos/AddVideoForm";
 
 const App: React.FC = () => {
   return (
@@ -12,7 +13,7 @@ const App: React.FC = () => {
           <Typography variant="h6">Video manager</Typography>
         </header>
         <Route path="/" exact component={Main} />
-        <Route path="/add" component={Main} />
+        <Route path="/add" component={AddVideoForm} />
       </div>
     </Router>
   );
